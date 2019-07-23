5b2e81c297b896574be484c009b29d1f77cdd485
YeJ XU <nhxyj111@gmail.com>
2019-07-23 19:03:28 +0800

    scalar + directive

--- index.js
+++ index.js
@@ -4,15 +4,77 @@ const {
   PubSub,
   AuthenticationError,
   UserInputError,
+  withFilter,
+  SchemaDirectiveVisitor,
 } = require('apollo-server')
+const { GraphQLScalarType, defaultFieldResolver } = require('graphql')
+const { Kind } = require('graphql/language')
+const GraphQLJSON = require('graphql-type-json')
+
+class UpperCaseDirective extends SchemaDirectiveVisitor {
+  visitFieldDefinition(field) {
+    const { resolve = defaultFieldResolver } = field
+    field.resolve = async function(...args) {
+      const result = await resolve.apply(this, args)
+      if (typeof result === 'string') {
+        return result.toUpperCase()
+      }
+      return result
+    }
+  }
+}
 
 const pubsub = new PubSub()
 const SOMETHING_CHANGED_TOPIC = 'something_changed'
 
 const typeDefs = gql`
+  directive @upper on FIELD_DEFINITION
+
+  scalar JSON
+  scalar Date
+
+  union Result = Book | Author
+
+  type Book {
+    title: String
+  }
+
+  type Author {
+    name: String
+  }
+
+  type Class {
+    name: String
+  }
+
+  type Color {
+    name: String
+  }
+
+  interface Work {
+    title: String
+    author: Author
+  }
+
+  type TextWork implements Work {
+    title: String
+    author: Author
+    classes: [Class]
+  }
+
+  type ColoringWork implements Work {
+    title: String
+    author: Author
+    colors: [Color]
+  }
+
   type Query {
     hello: String
     authenticationError: String
+    jsonTest: JSON
+    search: [Result]
+    schoolWorks: [Work]
+    directiveTest: String @upper
   }
 
   type Mutation {
@@ -25,11 +87,60 @@ const typeDefs = gql`
 `
 
 const resolvers = {
+  JSON: GraphQLJSON,
+
+  Result: {
+    __resolveType(obj, context, info) {
+      if (obj.name) {
+        return 'Author'
+      }
+      if (obj.title) {
+        return 'Book'
+      }
+      return null
+    },
+  },
+
+  Work: {
+    __resolveType(work, context, info) {
+      if (work.classes) {
+        return 'TextWork'
+      }
+      if (work.colors) {
+        return 'ColoringWork'
+      }
+      return null
+    },
+  },
+
+  Date: new GraphQLScalarType({
+    name: 'Date',
+    description: 'Date custom scalar type',
+    parseValue(value) {
+      return new Date(value)
+    },
+    serialize(value) {
+      return value.getTime()
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return parseInt(ast.value, 10)
+      }
+      return null
+    },
+  }),
+
   Query: {
     hello: () => 'hello',
     authenticationError: (parent, args, context) => {
       throw new AuthenticationError('must authenticate')
     },
+    jsonTest: () => ({ test: 'json type' }),
+    search: () => [{ name: 'test' }],
+    schoolWorks: () => [
+      { title: 'test', author: { name: 'a' }, colors: [{ name: 'red' }] },
+    ],
+    directiveTest: () => 'abc',
   },
 
   Mutation: {
@@ -44,7 +155,10 @@ const resolvers = {
 
   Subscription: {
     newMessage: {
-      subscribe: () => pubsub.asyncIterator(SOMETHING_CHANGED_TOPIC),
+      subscribe: withFilter(
+        () => pubsub.asyncIterator(SOMETHING_CHANGED_TOPIC),
+        (payload, variables, context, info) => true
+      ),
     },
   },
 }
@@ -67,6 +181,14 @@ const server = new ApolloServer({
     // Otherwise return the original error.  The error can also
     // be manipulated in other ways, so long as it's returned.
     return err
+  },
+  subscriptions: {
+    onConnect: (connectionParams, webSocket) => {
+      // throw new Error('Missing auth token!')
+    },
+  },
+  schemaDirectives: {
+    upper: UpperCaseDirective,
   },
 })
 
